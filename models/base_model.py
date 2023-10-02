79135b3523252768a9e1c43823845312d8086402
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-10-02 15:12:55 +0100

    Task 14 done

--- models/base_model.py
+++ models/base_model.py
@@ -59,13 +59,15 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """Returns a dictionary representation of the instance."""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+        if models.storage_t != 'db':
+            new_dict.pop('password', None)
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
--- models/user.py
+++ models/user.py
@@ -6,10 +6,11 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """Representation of a user"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -25,5 +26,18 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """Initializes user"""
         super().__init__(*args, **kwargs)
+
+        if 'password' in kwargs:
+            self.password = hashlib.md5
+            (kwargs['password'].encode()).hexdigest()
+
+    def to_dict(self):
+        """Returns a dictionary representation of User,
+            excluding 'password'.
+        """
+        user_dict = super().to_dict()
+        if 'password' in user_dict and models.storage_t != 'db':
+            del user_dict['password']
+        return user_dict
