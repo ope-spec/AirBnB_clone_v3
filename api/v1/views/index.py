b8cfded4158af5a657fa823d55ed939059f739b1
ConquerorCletus <smartchibuike2019@gmail.com>
2023-10-03 04:23:01 +0100

    included other classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,12 @@ import os
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 app = Flask(__name__)
 
 
